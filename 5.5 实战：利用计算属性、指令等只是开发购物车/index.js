911756a26f5204fb44d99bfd6e679177de64cfd8
ntk1986 <394808619@qq.com>
2023-10-01 18:30:15 +0800

    购物车功能开发。

---
+++ 5.5 实战：利用计算属性、指令等只是开发购物车/index.js
@@ -0,0 +1,52 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		// 在实际业务中，这个列表是通过Ajax从服务端动态获取的，这里只做示例，构建列表数据。
+		list: [{
+				id: 1,
+				name: 'iphone 7',
+				price: 6188,
+				count: 1
+			},
+			{
+				id: 2,
+				name: 'iPad Pro',
+				price: 5888,
+				count: 1
+			},
+			{
+				id: 3,
+				name: 'MacBook Pro',
+				price: 21488,
+				count: 1
+			}
+		],
+		pickedList: [],
+		selectAllChecked: false
+	},
+	computed: {
+		selectedMoney: function() {
+			var selectedMoney = 0;
+			for (var i = 0; i < this.pickedList.length; i++) {
+				var index = this.pickedList[i];
+				selectedMoney += this.list[index].price * this.list[index].count;
+			}
+			return selectedMoney;
+		}
+
+	},
+	methods: {
+		handleReduce: function(index) {
+			// 防御性代码，当数量为1时，禁止减少，直接返回。
+			if (this.list[index].count === 1) return;
+			this.list[index].count--;
+		},
+		handleAdd: function(index) {
+			this.list[index].count++;
+		},
+		handleRemove: function(index) {
+			// splice(index:number, num:number), index：需要删除元素的下标，num：删除元素的个数
+			this.list.splice(index, 1);
+		}
+	}
+})
---
+++ 5.5 实战：利用计算属性、指令等只是开发购物车/readme.md
@@ -0,0 +1,6 @@
+index.html
+	引入资源及模板
+index.js
+	Vue实例及业务代码
+style.css
+	样式
