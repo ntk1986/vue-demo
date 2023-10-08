8f08e0dafcd1f1c4c87418ce0fecf8717158bac6
ntk1986 <394808619@qq.com>
2023-10-08 11:22:06 +0800

    标签页组件bug fix: 修复关键词拼写错误，调整页面格式。

--- 7 组件详解/7.7 实战：两个常用组件的开发/7.7.2 开发一个标签页组件/tabs.js
+++ 7 组件详解/7.7 实战：两个常用组件的开发/7.7.2 开发一个标签页组件/tabs.js
@@ -1,6 +1,6 @@
 Vue.component('tabs', {
-	name: 'tabs',
-	template: '\
+    name: 'tabs',
+    template: '\
 		<div class = "tabs">\
 			<div class="tabs-bar">\
 				<div \
@@ -15,76 +15,76 @@ Vue.component('tabs', {
 			</div>\
 		</div>\
 	',
-	props: {
-		value: {
-			type: [String, Number]
-		}
-	},
-	data: function() {
-		return {
-			currentValue: this.value,
-			navList: []
-		}
-	},
-	methods: {
-		tabCls: function(item) {
-			return [
-				'tabs-tab',
-				{
-					'tabs-tab-active': item.name === this.currentValue
-				}
-			]
-		},
-		getPanes: function() {
-			// 获取所有name=“pane”的子节点。
-			return this.$children.filter(function(item) {
-				return item.$option.name === 'pane';
-			});
-		},
-		updateNav: function() {
-			// 清空navList
-			this.navList = [];
-			_this = this;
+    props: {
+        value: {
+            type: [String, Number]
+        }
+    },
+    data: function () {
+        return {
+            currentValue: this.value,
+            navList: []
+        }
+    },
+    methods: {
+        tabCls: function (item) {
+            return [
+                'tabs-tab',
+                {
+                    'tabs-tab-active': item.name === this.currentValue
+                }
+            ]
+        },
+        getPanes: function () {
+            // 获取所有name=“pane”的子节点。
+            return this.$children.filter(function (item) {
+                return item.$options.name === 'pane';
+            });
+        },
+        updateNav: function () {
+            // 清空navList
+            this.navList = [];
+            _this = this;
 
-			// 获取所有pane，填充navList
-			this.getPanes().forEach(function(pane, index) {
-				_this.navList.push({
-					label: pane.label,
-					name: pane.name || index
-				});
+            // 获取所有pane，填充navList
+            this.getPanes().forEach(function (pane, index) {
+                _this.navList.push({
+                    label: pane.label,
+                    name: pane.name || index
+                });
 
-				if (!pane.name) pane.name = index;
-				if (index === 0) {
-					if (!_this.currentValue) {
-						_this.currentValue = pane.name || index;
-					}
-				}
-			});
-			this.updateStatus();
-		},
-		updateStatus: function() {
-			var panes = this.getPanes();
-			var _this = this;
+                if (!pane.name) pane.name = index;
+                if (index === 0) {
+                    if (!_this.currentValue) {
+                        _this.currentValue = pane.name || index;
+                    }
+                }
+            });
+            this.updateStatus();
+        },
+        updateStatus: function () {
+            var panes = this.getPanes();
+            var _this = this;
 
-			panes.forEach(function(pane) {
-				return pane.show = pane.name === _this.currentValue;
-			});
-		},
-		handleChange: function(index) {
-			var nav = this.navList[index];
-			var name = nav.name;
+            panes.forEach(function (pane) {
+                return pane.show = pane.name === _this.currentValue;
+            });
+        },
+        handleChange: function (index) {
+            var nav = this.navList[index];
+            var name = nav.name;
 
-			this.currentValue = name;
-			this.$emit('input', name);
-			this.$emit('on-click', name);
-		}
-	},
-	watch: {
-		value: function(val) {
-			this.currentValue = val;
-		},
-		currentValue: function() {
-			this.updateStatus();
-		}
-	}
+            this.currentValue = name;
+            this.$emit('input', name);
+            this.$emit('on-click', name);
+        }
+    },
+    watch: {
+        value: function (val) {
+            this.currentValue = val;
+        },
+        currentValue: function () {
+            this.updateStatus();
+        }
+    }
 })
